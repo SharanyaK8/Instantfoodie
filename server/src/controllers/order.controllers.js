dd36eb31e6ea9ed8542397be1365e82c000ca48a
atharv7237 <atharvsapale90@email.com>
2026-07-10 13:25:56 +0530

    Order Tracking Pipeline: Completed.

--- server/src/controllers/order.controllers.js
+++ server/src/controllers/order.controllers.js
@@ -1,6 +1,8 @@
 import crypto from 'crypto'
 import Order from '../models/order.js';
 import foodItems from '../models/foodItem.js';
+import Restaurant from '../models/restaurant.js';
+import { get } from 'http';
 export const PlaceOrder = async (req, res) => {
     try {
         let { restaurantId, items, deliveryAddress } = req.body; // items return array consisting of objects returning foodItem id and quantity
@@ -22,26 +24,26 @@ export const PlaceOrder = async (req, res) => {
         const orderId = `ORD-${crypto.randomBytes(4).toString('hex').toUpperCase()}`
 
         let orderItems = []
-        
-        for(let item of items){
+
+        for (let item of items) {
             let findItem = await foodItems.findById(item.foodItemId)
             if (!findItem) {
                 return res.status(404).json({
                     success: false,
                     message: "No food item found",
-                    
+
                 })
             }
 
-            if(!findItem.isAvailable){
+            if (!findItem.isAvailable) {
                 return res.status(404).json({
                     success: false,
                     message: " Some items are out of stock",
-                    
+
                 })
             }
 
-            orderItems.push ({
+            orderItems.push({
                 foodItemId: item.foodItemId,
                 name: findItem.name,
                 price: findItem.price,
@@ -79,3 +81,122 @@ export const PlaceOrder = async (req, res) => {
     }
 }
 
+export const getRestaurantOrders = async (req, res) => {
+    try {
+        const user = req.user
+        if (user.role !== 'restaurant') {
+            return res.status(403).json({
+                success: false,
+                message: "only restaurants can view there orders",
+            })
+        }
+
+        const getRestaurant = await Restaurant.findOne({ owner: user._id })
+        if (!getRestaurant) {
+            return res.status(404).json({
+                success: false,
+                message: "Restaurant not found"
+            })
+        }
+
+        const getOrders = await Order.find({ restaurantId: getRestaurant._id })
+
+        if (getOrders.length === 0) {
+            return res.status(200).json({
+                success: true,
+                message: "No orders are placed"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            Orders: getOrders
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+export const updateOrderStatus = async (req, res) => {
+    try {
+        const { orderId } = req.params; // pass the generated order id like ORD-7898G3
+        const { orderStatus } = req.body
+
+        if (!orderId) {
+            return res.status(400).json({
+                success: false,
+                message: "Need to send order id first",
+            })
+        }
+
+
+        if (!orderStatus) {
+            return res.status(400).json({
+                success: false,
+                message: "Need to send status",
+            })
+        }
+
+        const user = req.user
+        if (user.role !== 'restaurant') {
+            return res.status(403).json({
+                success: false,
+                message: "only restaurants can update there order details",
+            })
+        }
+
+        const restaurant = await Restaurant.findOne({owner:user._id})
+        if (!restaurant) {
+            return res.status(403).json({
+                success: false,
+                message: "Restuarant not found"
+            })
+        }
+        
+        const order = await Order.findOne({ orderId })
+
+        if (!order) {
+            return res.status(403).json({
+                success: false,
+                message: "No order of this id is avalible",
+            })
+        }
+
+        if (order.restaurantId.toString === restaurant._id) {
+            return res.status(400).json({
+                success: false,
+                message: "Unauthorized access",
+            })
+        }
+
+        if (order.orderStatus === "Delivered" || order.orderStatus === "Shipped") {
+            return res.status(400).json({
+                success: false,
+                message: "Order is already Completed",
+            })
+        }
+
+        const updateStatus = await Order.findByIdAndUpdate(
+            order._id,
+            {orderStatus},
+            { new: true, runValidators: true }
+        )
+
+        return res.status(200).json({
+            success: true,
+            Status:updateStatus 
+        });
+
+    } catch (error) {
+        return res
+            .status(500)
+            .json({
+                error: error.message,
+                success: false,
+            });
+    }
+}
--- server/src/routes/order.route.js
+++ server/src/routes/order.route.js
@@ -1,8 +1,12 @@
 import { Router } from "express"; 
-import { PlaceOrder } from "../controllers/order.controllers.js";
+import { PlaceOrder , getRestaurantOrders , updateOrderStatus} from "../controllers/order.controllers.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 const router = Router()
 
 router.post('/placeOrder', authMiddleware ,PlaceOrder)
+
+router.get('/restaurants',authMiddleware , getRestaurantOrders)
+
+router.patch('/:orderId' , authMiddleware , updateOrderStatus)
 
 export default router;
